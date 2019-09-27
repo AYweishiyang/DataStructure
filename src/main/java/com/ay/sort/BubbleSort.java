f30d3383b432d1b8eb077be6e3ff52615424976e
ay_glb <960484339@qq.com>
2019-09-27 16:02:32 +0800

    selectsort

--- src/main/java/com/ay/sort/BubbleSort.java
+++ src/main/java/com/ay/sort/BubbleSort.java
@@ -9,19 +9,49 @@ import java.util.Arrays;
 //时间复杂度O(n^2)
 public class BubbleSort {
     public static void main(String[] args) {
+
+//        int[] nums = new int[]{3,2,4};
+//        int[] index = new int[2];
+//        index = twoSum(nums, 47);
+//        System.out.println(Arrays.toString(index));
+        System.out.println(isPalindrome(100));
 //        int[] arr = {3,9,-1,10,-2,20,30,40,50};
-        int[] arr = new int[80000];
-        for (int i = 0; i < 80000; i++) {
-            arr[i] = (int)(Math.random() * 8000000);
-        }
-        long startTime = System.currentTimeMillis();
-        bubbleSort(arr);
-        long endTime = System.currentTimeMillis();
-        System.out.println((endTime-startTime)/1000.0 + " s");
+//        int[] arr = new int[80000];
+//        for (int i = 0; i < 80000; i++) {
+//            arr[i] = (int)(Math.random() * 8000000);
+//        }
+//        long startTime = System.currentTimeMillis();
+//        bubbleSort(arr);
+//        long endTime = System.currentTimeMillis();
+//        System.out.println((endTime-startTime)/1000.0 + " s");
 //        System.out.println(Arrays.toString(arr));
 
     }
+    public static boolean isPalindrome(int x) {
 
+            if(x>=0){
+                String str = String.valueOf(x);
+                for (int i = 0; i < str.length() / 2 +1; i++) {
+                    if(str.charAt(i) != str.charAt(str.length()-i-1)){
+                        return false;
+                    }
+                }
+                return true;
+            }
+            return false;
+
+    }
+
+    public static int[] twoSum(int[] nums, int target) {
+        for(int i = 0;i < nums.length; i++){
+            for(int j = 0;j < nums.length;j++){
+                if(nums[i] + nums[j] ==target){
+                    return new int[]{i,j};
+                }
+            }
+        }
+        return new int[]{-1,-1};
+    }
     public static void bubbleSort1(int[] arr){
         for (int i = 0; i < arr.length - 1; i++) {
             boolean flag = false;
@@ -78,4 +108,8 @@ class LinkList{
     public static void add(LinkNode node){
 
     }
+}
+
+class Solution {
+
 }
