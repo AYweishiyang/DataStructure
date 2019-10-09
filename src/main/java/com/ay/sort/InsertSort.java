17ea0d690c88ddfb983869a7b449a71d8b81603e
ay_glb <960484339@qq.com>
2019-10-09 22:20:29 +0800

    insertSort

--- src/main/java/com/ay/sort/InsertSort.java
+++ src/main/java/com/ay/sort/InsertSort.java
@@ -13,14 +13,14 @@ public class InsertSort {
             arr[i] = (int)(Math.random() * 80000000);
         }
         long startTime = System.currentTimeMillis();
-        insertSort(arr);
+        insertSort1(arr);
         long endTime = System.currentTimeMillis();
         System.out.println((endTime-startTime)/1000.0 + " s");
     }
 
     public static void insertSort(int[] arr) {
-        int insertVal = 0;
-        int insertIndex = 0;
+        int insertVal;
+        int insertIndex;
         for (int i = 1; i < arr.length; i++) {
             insertVal = arr[i];
             insertIndex = i - 1;
@@ -30,6 +30,23 @@ public class InsertSort {
             }
             if(insertIndex + 1 !=i){
                 arr[insertIndex + 1] = insertVal;
+            }
+
+        }
+
+    }
+    public static void insertSort1(int[] arr){
+        int insertValue;
+        int insertIndex;
+        for (int i = 1; i < arr.length; i++) {
+            insertIndex = i - 1;
+            insertValue = arr[i];
+            while(insertIndex >=0 && insertValue < arr[insertIndex]){
+                arr[insertIndex+1] = arr[insertIndex];
+                insertIndex--;
+            }
+            if(insertIndex+1 != i ){
+                arr[insertIndex+1] = insertValue;
             }
 
         }
