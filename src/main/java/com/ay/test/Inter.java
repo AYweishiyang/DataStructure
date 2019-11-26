91337e7adac94527825aa87714d03c29922aa750
ay_glb <960484339@qq.com>
2019-11-26 23:32:55 +0800

    加注释

---
+++ src/main/java/com/ay/test/Inter.java
@@ -0,0 +1,9 @@
+package com.ay.test;
+
+/**
+ * @author ay
+ * @create 2019-11-26 22:13
+ */
+public interface Inter {
+    void f();
+}
---
+++ src/main/java/com/ay/test/InterImpl.java
@@ -0,0 +1,12 @@
+package com.ay.test;
+
+/**
+ * @author ay
+ * @create 2019-11-26 22:13
+ */
+public class InterImpl implements Inter {
+    @Override
+    public void f() {
+
+    }
+}
