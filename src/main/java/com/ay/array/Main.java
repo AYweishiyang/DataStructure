063aa06758f930dbc966383a3f10c9c77e474bce
ay_glb <960484339@qq.com>
2019-12-03 23:16:45 +0800

    LinkedList.java

--- src/main/java/com/ay/array/Main.java
+++ src/main/java/com/ay/array/Main.java
@@ -1,16 +1,19 @@
 package com.ay.array;
 
+import com.ay.link.LinkedList;
+
 /**
  * @author ay
  * @create 2019-11-25 15:31
  */
 public class Main {
     public static void main(String[] args) {
-        LoopQueue<Integer> queue = new LoopQueue<>();
-        for (int i = 0; i < 20; i++) {
-            queue.enqueue(i);
-            System.out.println(queue);
+        LinkedList<Integer> linkedList = new LinkedList<>();
+        for (int i = 0; i < 5; i++) {
+            linkedList.addFirst(i);
+            System.out.println(linkedList);
         }
-
+        linkedList.add(2,66);
+        System.out.println(linkedList);
     }
 }
