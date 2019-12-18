c0b0609466db9b4f7e494ac820132efe3965cd07
ay_glb <960484339@qq.com>
2019-12-18 17:28:15 +0800

    jvm

--- src/main/java/com/ay/test01/TestString.java
+++ src/main/java/com/ay/test01/TestString.java
@@ -1,16 +1,30 @@
 package com.ay.test01;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * @author ay
  * @create 2019-09-25 16:10
  */
 public class TestString {
-    public static void main(String[] args) {
-        String s1 = "javaEE";
-        String s2 = "javaEE";
-        s2 = "hello";
-        System.out.println(s1);
+    public static void main(String[] args) throws InterruptedException {
+//        String s1 = "javaEE";
+//        String s2 = "javaEE";
+//        s2 = "hello";
+//        System.out.println(s1);
+//        Thread.sleep(Integer.MAX_VALUE);
+        int i = 0;
+        List<String> list = new ArrayList<>();
 
-
+        try{
+            while(true){
+                list.add(String.valueOf(++i).intern());
+            }
+        }catch (Throwable e){
+            System.out.println("***************i:"+i);
+            e.printStackTrace();
+            throw e;
+        }
     }
 }
