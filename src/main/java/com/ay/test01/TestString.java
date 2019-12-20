5e0994fa0a10446b5e4a9fd5e1e8e54a6d55ba04
ay_glb <960484339@qq.com>
2019-12-20 16:20:12 +0800

    jvm

--- src/main/java/com/ay/test01/TestString.java
+++ src/main/java/com/ay/test01/TestString.java
@@ -9,22 +9,23 @@ import java.util.List;
  */
 public class TestString {
     public static void main(String[] args) throws InterruptedException {
-//        String s1 = "javaEE";
-//        String s2 = "javaEE";
-//        s2 = "hello";
-//        System.out.println(s1);
+        String s1 = "javaEE";
+        String s2 = "javaEE";
+        s2 = "hello";
+        System.out.println(s1);
+        System.out.println("git");
 //        Thread.sleep(Integer.MAX_VALUE);
-        int i = 0;
-        List<String> list = new ArrayList<>();
-
-        try{
-            while(true){
-                list.add(String.valueOf(++i).intern());
-            }
-        }catch (Throwable e){
-            System.out.println("***************i:"+i);
-            e.printStackTrace();
-            throw e;
-        }
+//        int i = 0;
+//        List<String> list = new ArrayList<>();
+//
+//        try{
+//            while(true){
+//                list.add(String.valueOf(++i).intern());
+//            }
+//        }catch (Throwable e){
+//            System.out.println("***************i:"+i);
+//            e.printStackTrace();
+//            throw e;
+//        }
     }
 }
