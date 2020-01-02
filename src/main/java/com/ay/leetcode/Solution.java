7b6a23e5474c8d3fa1535303277b4b451f40898b
ay_glb <960484339@qq.com>
2020-01-02 16:52:21 +0800

    heap

--- src/main/java/com/ay/leetcode/Solution.java
+++ src/main/java/com/ay/leetcode/Solution.java
@@ -1,6 +1,9 @@
 package com.ay.leetcode;
 
-
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.TreeMap;
+import java.util.TreeSet;
 import com.ay.array.ArrayStack;
 
 /**
@@ -8,8 +11,65 @@ import com.ay.array.ArrayStack;
  * @create 2019-11-30 11:16
  */
 public class Solution {
-    public int uniqueMorseRepresentations(String[] words) {
+    //350
+    public int[] intersect(int[] nums1, int[] nums2) {
+        TreeMap<Integer,Integer> map = new TreeMap<>();
+        for(int num : nums1){
+            if(map.containsKey(num)){
+                map.put(num,map.get(num)+1);
+            }else{
+                map.put(num,1);
+            }
+        }
+        ArrayList<Integer> list = new ArrayList<>();
+        for(int num:nums2){
+            if(map.containsKey(num)){
+                map.put(num,map.get(num)-1);
+                list.add(num);
+                if(map.get(num)==0){
+                    map.remove(num);
+                }
+            }
+        }
+        int[] ret =new int[list.size()];
+        for(int i=0;i<list.size();i++){
+            ret[i] = list.get(i);
+        }
+        return ret;
+    }
+    //349
+    public int[] intersection(int[] nums1, int[] nums2) {
+        HashSet<Integer> set = new HashSet<>();
+        for(int num : nums1){
+            set.add(num);
+        }
+        ArrayList<Integer> list = new ArrayList<>();
+        for(int num:nums2){
+            if(set.contains(num)){
+                list.add(num);
+                set.remove(num);
+            }
 
+        }
+        int[] ret = new int[list.size()];
+        for(int i = 0; i < list.size(); i ++){
+            ret[i] = list.get(i);
+        }
+        return ret;
+    }
+    public int uniqueMorseRepresentations(String[] words) {
+        String[] codes = {".-","-...","-.-.","-..",".","..-.","--.","....","..",".---","-.-",".-..","--","-.","---",".--.","--.-",".-.","...","-","..-","...-",".--","-..-","-.--","--.."};
+        TreeSet<String> treeSet = new TreeSet<>();
+
+        for(String word : words){
+            StringBuilder res = new StringBuilder();
+            for (int i = 0; i < word.length(); i++) {
+                res.append(codes[word.charAt(i)-'a']);
+            }
+            treeSet.add(res.toString());
+        }
+
+        return treeSet.size();
     }
 
     public static boolean isValid(String s) {
