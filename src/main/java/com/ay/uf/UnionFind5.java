8335bfc8859a2ce979b3797d3a8373a0d2d6df91
ay_glb <960484339@qq.com>
2020-01-08 17:39:59 +0800

    unionFind

---
+++ src/main/java/com/ay/uf/UnionFind5.java
@@ -0,0 +1,78 @@
+package com.ay.uf;
+
+/**
+ * @author ay
+ * @create 2020-01-08 15:45
+ */
+//路径压缩
+// 我们的第4版Union-Find
+public class UnionFind5 implements UF{
+
+    private int[] parent; // parent[i]表示第一个元素所指向的父节点
+    private int[] rank;     // sz[i]表示以i为根的集合中元素个数
+
+    // 构造函数
+    public UnionFind5(int size){
+
+        parent = new int[size];
+        rank = new int[size];
+
+        // 初始化, 每一个parent[i]指向自己, 表示每一个元素自己自成一个集合
+        for(int i = 0 ; i < size ; i ++){
+            parent[i] = i;
+            rank[i] = 1;
+        }
+    }
+
+    @Override
+    public int getSize(){
+        return parent.length;
+    }
+
+    // 查找过程, 查找元素p所对应的集合编号
+    // O(h)复杂度, h为树的高度
+    private int find(int p){
+        if(p < 0 || p >= parent.length)
+            throw new IllegalArgumentException("p is out of bound.");
+
+        // 不断去查询自己的父亲节点, 直到到达根节点
+        // 根节点的特点: parent[p] == p
+        while( p != parent[p] )
+            p = parent[parent[p]];//路径压缩
+            p = parent[p];
+        return p;
+    }
+
+    // 查看元素p和元素q是否所属一个集合
+    // O(h)复杂度, h为树的高度
+    @Override
+    public boolean isConnected( int p , int q ){
+        return find(p) == find(q);
+    }
+
+    // 合并元素p和元素q所属的集合
+    // O(h)复杂度, h为树的高度
+    @Override
+    public void unionElements(int p, int q){
+
+        int pRoot = find(p);
+        int qRoot = find(q);
+
+        if(pRoot == qRoot)
+            return;
+
+        // 根据两个元素所在树的元素个数不同判断合并方向
+        // 将元素个数少的集合合并到元素个数多的集合上
+        if(rank[pRoot] < rank[qRoot]){
+            parent[pRoot] = qRoot;
+
+        }
+        else if(rank[pRoot] > rank[qRoot]){ // sz[qRoot] <= sz[pRoot]
+            parent[qRoot] = pRoot;
+        }
+        else {  // rank[pRoot] == rank[qRoot]
+            parent[pRoot] = qRoot;
+            rank[qRoot] += rank[qRoot];
+        }
+    }
+}
