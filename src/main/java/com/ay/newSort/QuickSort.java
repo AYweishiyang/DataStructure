f147d0ee33b2bd2fee8541b1e711fa10b43780cc
ay_glb <960484339@qq.com>
2020-02-18 22:13:05 +0800

    QuickSort

--- src/main/java/com/ay/newSort/QuickSort.java
+++ src/main/java/com/ay/newSort/QuickSort.java
@@ -7,6 +7,9 @@ import java.util.Arrays;
 /**
  * @author ay
  * @create 2020-02-18 11:19
+ * 取 arr[l] 作为切分元素，然后从数组的左端向右扫描直到找到第一个大于等于它的元素，再从数组的右端向左扫描找到第一个小于它的元素，
+ * 交换这两个元素。不断进行这个过程，就可以保证左指针 i 的左侧元素都不大于切分元素，右指针 j 的右侧元素都不小于切分元素。
+ * 当两个指针相遇时，将切分元素 a[l] 和 a[j] 交换位置。
  */
 public class QuickSort<T extends Comparable<T>> implements Sort<T>{
     public static void main(String[] args) {
@@ -29,7 +32,7 @@ public class QuickSort<T extends Comparable<T>> implements Sort<T>{
         int j = right+1;
         T pivot = arr[left];
         while(true){
-            while(less(arr[++i],pivot) && i != right);
+            while(less(arr[++i],pivot) && i != right);//防止越界
             while(less(pivot,arr[--j]) && j != left);
             if(i>=j){
                 break;
