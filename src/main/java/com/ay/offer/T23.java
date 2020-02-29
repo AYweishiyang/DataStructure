8502aa24cb90eed9fb682d72fba78808c4694a45
ay_glb <960484339@qq.com>
2020-02-29 21:34:30 +0800

    EntryNodeOfLoop

---
+++ src/main/java/com/ay/offer/T23.java
@@ -0,0 +1,68 @@
+package com.ay.offer;
+
+/**
+ * @author ay
+ * @create 2020-02-29 11:34
+ * 给一个链表，若其中包含环，请找出该链表的环的入口结点，否则，输出null。
+ *
+ * 思路
+ * 1 确定是否包含环，找到一个在环内的节点，该节点用于计算环的节点个数K
+ * 2 双指针 第一个指针先走K,然后
+ */
+public class T23 {
+
+    public ListNode EntryNodeOfLoop(ListNode pHead)
+    {
+        ListNode meet = meetingNode(pHead);
+        if(meet == null){
+            return null;
+        }
+        ListNode first = pHead;
+        while(first != meet){
+            first = first.next;
+            meet = meet.next;
+        }
+        return first;
+    }
+    public ListNode EntryNodeOfLoop2(ListNode pHead){
+        //得到相遇点
+        ListNode meet = meetingNode(pHead);
+        if(meet == null){
+            return null;
+        }
+
+//      得到环中的节点个数
+        int nodesInLoop=1;
+        ListNode p1=meet;
+        while(p1.next!=meet){
+            p1=p1.next;
+            ++nodesInLoop;
+        }
+//      移动p1
+        p1=pHead;
+        for(int i=0;i<nodesInLoop;i++){
+            p1=p1.next;
+        }
+//      移动p1，p2
+        ListNode p2=pHead;
+        while(p1!=p2){
+            p1=p1.next;
+            p2=p2.next;
+        }
+        return p1;
+    }
+    private ListNode meetingNode(ListNode head){
+        ListNode slow = head;
+        ListNode fast = head;
+
+        while(fast != null&& fast.next != null){
+            slow = slow.next;
+            fast = fast.next.next;
+            if(slow == fast){
+                return slow;
+            }
+
+        }
+        return null;
+    }
+}
