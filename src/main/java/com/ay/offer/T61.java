5904edb3ac78addc41991f387f877e1b946d766d
ay_glb <960484339@qq.com>
2020-05-21 20:37:25 +0800

    添加注释

--- src/main/java/com/ay/offer/T61.java
+++ src/main/java/com/ay/offer/T61.java
@@ -6,39 +6,40 @@ package com.ay.offer;
  * 扑克牌中的顺子
  */
 public class T61 {
-    public static boolean isContinuous(int [] numbers) {
-        if(numbers == null || numbers.length == 0){
+    public static boolean isContinuous(int[] numbers) {
+        if (numbers == null || numbers.length == 0) {
             return false;
         }
-        int[]d = new int[14];
-        d[0] = 0;
+        int[] d = new int[14];
+        d[0] = -5;
         int len = numbers.length;
         int max = -1;
         int min = 14;
-        for(int i =0;i<len;i++){
+        for (int i = 0; i < len; i++) {
             d[numbers[i]]++; //记录每张牌出现次数，索引位置为牌的大小
-            if(numbers[i] == 0){//min ,max 都不记0
+            if (numbers[i] == 0) {//min ,max 都不记0
                 continue;
             }
-            if(d[numbers[i]]>1){//大于1证明出现对子
+            if (d[numbers[i]] > 1) {//大于1证明出现对子
                 return false;
             }
             //找出numbers中的最大、最小值
-            if(numbers[i] >max){
+            if (numbers[i] > max) {
                 max = numbers[i];
-            } if(numbers[i] <min){
+            }
+            if (numbers[i] < min) {
                 min = numbers[i];
             }
 
         }
-        if(max -min<5 && d[0] <=2){
+        if (max - min < 5 && d[0] <= 2) {
             return true;
         }
         return false;
     }
 
     public static void main(String[] args) {
-        int[] numbers = new int[]{0,0,0,4,5};
+        int[] numbers = new int[]{0, 0, 0, 4, 5};
         System.out.println(isContinuous(numbers));
     }
 }
