c33fbe2431a2883f8e8d43083bc4294193ddde0d
ay_glb <960484339@qq.com>
2020-06-04 15:29:35 +0800

    leetcode

---
+++ src/main/java/com/ay/leetcode/queueandstack/TreeNode.java
@@ -0,0 +1,15 @@
+package com.ay.leetcode.queueandstack;
+
+/**
+ * @author ay
+ * @create 2020-03-03 16:47
+ */
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode(int x) {
+        val = x;
+    }
+}
